ce02b097730f1c3ae1b4a45705be95b63bb0e6d5
AlexCanDoIt <alexcandoitt@gmsil.com>
2021-01-24 17:33:25 +0200

    Update task-8.js

--- js/task-8.js
+++ js/task-8.js
@@ -7,13 +7,23 @@ const boxesConteinerRef = document.querySelector('#boxes');
 const getRandomNumber = (min, max) => Math.round(Math.random() * (max - min) + min);
 const getRandomRGB = () => `rgb(${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)}, ${getRandomNumber(0, 255)})`;
 
+// const createBoxes = amount => {
+//   for (let i = 0; i < amount; i += 1) {
+//     const boxRef = document.createElement('div');
+
+//     boxRef.style.backgroundColor = getRandomRGB();
+//     boxRef.style.width = `${30 + (i * 10)}px`;
+//     boxRef.style.height = `${30 + (i * 10)}px`;
+    
+//     boxesConteinerRef.append(boxRef);
+//   };
+// };
+
 const createBoxes = amount => {
   for (let i = 0; i < amount; i += 1) {
     const boxRef = document.createElement('div');
 
-    boxRef.style.backgroundColor = getRandomRGB();
-    boxRef.style.width = `${30 + (i * 10)}px`;
-    boxRef.style.height = `${30 + (i * 10)}px`;
+    boxRef.style.cssText = `width: ${30 + (i * 10)}px; height: ${30 + (i * 10)}px; background-color: ${getRandomRGB()};`
     
     boxesConteinerRef.append(boxRef);
   };
